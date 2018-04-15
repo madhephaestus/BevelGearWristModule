e362d42119ba62d4bb9c30b53d4cf98e61a47618
Kevin Harrington <mad.hephaestus@gmail.com>
2018-04-15 17:19:45 -0400

    inner bearing

--- Wrist.groovy
+++ Wrist.groovy
@@ -2,26 +2,33 @@ LengthParameter printerOffset 			= new LengthParameter("printerOffset",0.5,[1.2,
 
 double pinRadius = ((3/16)*25.4+printerOffset.getMM())/2
 
-double pinLength = (2.5*25.4)+ (printerOffset.getMM()*2)
-double linkMaterialThickness = pinLength/2
-double washerThickness = 1.5
-double washerOd = 17
-double washerId = 12.75
-double gearThickness =8
+double pinLength = (1.25*25.4)+ (printerOffset.getMM()*2)
 
 
+println "Pin len ="+pinLength
 if(args == null){
 	args=[
 		Vitamins.get("ballBearing","R8-60355K505"),
-		new Cylinder(pinRadius,pinRadius,pinLength,(int)30).toCSG().movez(-pinLength/2), // steel reenforcmentPin
+		new Cylinder(pinRadius,pinRadius,pinLength,(int)30).toCSG(), // steel reenforcmentPin
 		new Cylinder(pinRadius,pinRadius,pinLength,(int)30).toCSG().movez(-pinLength/2) // steel reenforcmentPin
 		
 	]
 }
-double encoderToEncoderDistance = pinLength-args[0].getMaxZ()*2
+double linkMaterialThickness = pinLength/2
+double washerThickness = 1.5
+double washerOd = 17
+double washerId = 12.75
+double bearingThickness = args[0].getMaxZ()
+double gearThickness =(pinLength -(bearingThickness*3 -washerThickness*2))/2
+
+double encoderToEncoderDistance =args[0].getMaxX()*2+
+					gearThickness*2 -
+					washerThickness*4 +
+					args[0].getMaxZ()*2+
+					bearingThickness*2
 double pitch = 5
-int aTeeth =    Math.PI*(encoderToEncoderDistance -washerThickness*2 )/pitch
-int bTeeth =    Math.PI*(args[0].getMaxX()+washerThickness+gearThickness)*2/pitch
+int aTeeth =    Math.PI*(encoderToEncoderDistance+washerThickness*2)/pitch
+int bTeeth =    Math.PI*(args[0].getMaxX()+washerThickness*2+gearThickness)*2/pitch 
 
 // call a script from another library
 List<Object> bevelGears = (List<Object>)ScriptingEngine
@@ -41,8 +48,8 @@ List<Object> spurGears = (List<Object>)ScriptingEngine
             "https://github.com/madhephaestus/GearGenerator.git", // git location of the library
             "bevelGear.groovy" , // file to load
             // Parameters passed to the funcetion
-            [	  aTeeth,// Number of teeth gear a
-	            bTeeth,// Number of teeth gear b
+            [	  bTeeth-2,// Number of teeth gear a
+	            1*(bTeeth-2),// Number of teeth gear b
 	            gearThickness,// thickness of gear A
 	            pitch,// gear pitch in arch length mm
 	           0,
@@ -76,16 +83,28 @@ CSG bdrive = bevelGears.get(1).union(drivenB)
 // return the CSG parts
 gears= [outputGear,adrive,bdrive]
 
+CSG innerBearing = args[0]
+			.toZMin()
+			.roty(-90)
+			.toXMax()
+			.movex( adrive.getMinX()-washerThickness)
+			.movez(bevelGears.get(3))
+
 CSG bearing =args[0]
 			.toZMin()
 			.roty(-90)
-			.movex(  encoderToEncoderDistance/2+gearThickness)
+			.movex(  encoderToEncoderDistance/2+gearThickness+washerThickness)
 			.movez(bevelGears.get(3))
-bearing=bearing.union(bearing.rotz(180))
-		.union(args[0].movez(gearThickness+washerThickness))
+bearing=CSG.unionAll([bearing,
+		bearing.rotz(180),
+		args[0].movez(gearThickness+washerThickness),
+		innerBearing
+		])
 CSG pin  = args[1]
 			.roty(-90)
 			.movez(bevelGears.get(3))
+			.toXMax()
+			.movex(  encoderToEncoderDistance/2+gearThickness+bearingThickness+washerThickness)
 CSG motor = 	args[2]
 			.roty(-90)
 			.movez(	distanceToMotor)
