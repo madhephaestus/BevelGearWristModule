3fa56406ed44ed0ece01be4f76e163bc031f2d3d
Kevin Harrington <mad.hephaestus@gmail.com>
2018-09-01 17:53:25 -0400

    adding units

--- Wrist.groovy
+++ Wrist.groovy
@@ -411,13 +411,13 @@ def nearestFive(def num){
 		return num
 	return num
 }
-println "Bottom to shaft "+ distanceToShaft
-println "Shaft to top  "+ upperDistLinkLen
-println "2x Top bolt     M5-"+ nearestFive(bracket.getTotalX()+actualMotorThickness*2+nut.getTotalZ())
-println "1x Center Bolt  M5-" +nearestFive(knuckelRigth.getMaxZ()+nut.getTotalZ())
-println "4x Knuckel Bolt M5-" +nearestFive(knuckelRigth.getTotalY()*2+nut.getTotalZ())
-println "8x Mount Bolt   M5-" +nearestFive(outputGear.getTotalZ()+bracket.getTotalZ()+nut.getTotalZ())
-println "2x Axil Bolt    M5-" +nearestFive(outerBearingDistance/2-3)
+println "Bottom to shaft "+ distanceToShaft+" mm"
+println "Shaft to top    "+ upperDistLinkLen+" mm"
+println "2x Top bolt     M5-"+ nearestFive(bracket.getTotalX()+actualMotorThickness*2+nut.getTotalZ())+" mm"
+println "1x Center Bolt  M5-" +nearestFive(knuckelRigth.getMaxZ()+nut.getTotalZ())+" mm"
+println "4x Knuckel Bolt M5-" +nearestFive(knuckelRigth.getTotalY()*2+nut.getTotalZ())+" mm"
+println "8x Mount Bolt   M5-" +nearestFive(outputGear.getTotalZ()+bracket.getTotalZ()+nut.getTotalZ())+" mm"
+println "2x Axil Bolt    M5-" +nearestFive(outerBearingDistance/2-3)+" mm"
 println "17x M5 lock nuts " 
 println "6x 695zz bearings " 
 
